fa4ad8612eb8c7912ae948e9e856f189479b74a6
Ashok2252 <ashokmali1999@gmail.com>
2023-10-19 18:52:17 +0530

    Style added to Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import jumboData from "./fixtures/jumbo.json";
 import Jumbotron from "./components/jumbotron";
+import "normalize.css";
 
 export default function App() {
   return (
     <>
       <Jumbotron.Container>
         {jumboData.map((item) => (
-          <Jumbotron key={item.id}>
-            <p>{item.title}</p>
-            <p>{item.subTitle}</p>
-            <p>{item.image}</p>
-            <p>{item.id}</p>
-            <p>{item.alt}</p>
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
           </Jumbotron>
         ))}
       </Jumbotron.Container>
