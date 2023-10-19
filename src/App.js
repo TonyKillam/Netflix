cb7f8384490c3c929a5e7319813ef45bf1a90422
Ashok2252 <ashokmali1999@gmail.com>
2023-10-19 17:05:59 +0530

    Raw text added at Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import React from "react";
+import jumboData from "./fixtures/jumbo.json";
+import Jumbotron from "./components/jumbotron";
+
 export default function App() {
-  return <p>Hola Netflix ;)</p>;
+  return (
+    <>
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id}>
+            <p>{item.title}</p>
+            <p>{item.subTitle}</p>
+            <p>{item.image}</p>
+            <p>{item.id}</p>
+            <p>{item.alt}</p>
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
+    </>
+  );
 }
