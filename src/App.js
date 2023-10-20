de9bc48b3413369cf14ea5962b04375a07c00bd2
Ashok2252 <ashokmali1999@gmail.com>
2023-10-20 16:06:21 +0530

    Footer Added and Styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { JumbotronContainer } from "./containers/jumbotron";
 import "normalize.css";
+import FooterContainer from "./containers/footer";
 
 export default function App() {
   return (
     <>
       <JumbotronContainer />
+      <FooterContainer />
     </>
   );
 }
