b14d514f1d25da893cf18d6f2cbce3d49bc0e379
Ashok2252 <ashokmali1999@gmail.com>
2023-10-26 11:25:42 +0530

    Added Firebase Context to the Index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GlobalStyles } from "./global-styles";
 import App from "./App";
-// import { firebase } from "./lib/firebase.prod";
+import { firebase } from "./lib/firebase.prod";
 import "normalize.css";
+import { FirebaseContext } from "./context/firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>
 );
