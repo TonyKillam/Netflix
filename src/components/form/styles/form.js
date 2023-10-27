05065c635b2c2c7c20170b69b4fcb5ac9eaa7a18
Ashok2252 <ashokmali1999@gmail.com>
2023-10-27 19:41:21 +0530

    Sign In Page finished

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -1,21 +1,26 @@
 import styled from "styled-components";
-import { Link as ReachRouterLink } from "react-router-dom";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  min-height: 660px;
+  min-height: 560px;
   background-color: rgba(0, 0, 0, 0.75);
   border-radius: 5px;
   box-sizing: border-box;
   width: 100%;
   margin: auto;
   max-width: 450px;
-  padding: 60px 68px 40px;
+  padding: 40px 68px 40px;
   margin-bottom: 100px;
 `;
 
-export const Base = styled.form``;
+export const Base = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 450px;
+  width: 100%;
+`;
 
 export const Error = styled.div`
   background: #e87c03;
@@ -27,19 +32,92 @@ export const Error = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-size: 50px;
+  font-size: 32px;
   color: #fff;
+  font-weight: bold;
+  margin-bottom: 28px;
 `;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+  font-size: 16px;
+  color: #737373;
+  font-weight: 500;
+`;
 
-export const TextSmall = styled.p``;
+export const TextSmall = styled.p`
+  margin-top: 10px;
+  font-size: 13px;
+  line-height: normal;
+  color: #8c8c8c;
+`;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  background: #333;
+  border-radius: 4px;
+  border: 0;
+  color: white;
+  height: 50px;
+  line-height: 50px;
+  padding: 5px 20px;
+  margin-bottom: 20px;
 
-export const Submit = styled.button``;
+  &:last-of-type {
+    margin-bottom: 30px;
+  }
+`;
 
-export const Link = styled(ReachRouterLink)`
+export const Submit = styled.button`
+  background: #e50914;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: bold;
+  margin: 24px 0 12px;
+  padding: 16px;
+  border: 0;
+  color: white;
+  cursor: pointer;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+
+  &:hover {
+    background: #8b0000;
+  }
+
+  &:active {
+    background: #ff0000;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+
+  @media (max-width: 600px) {
+    margin: 10px 0 20px;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 14px;
+    padding: 15px;
+  }
+
+  @media (max-width: 320px) {
+    font-size: 12px;
+    padding: 10px;
+  }
+
+  @media (max-width: 280px) {
+    font-size: 10px;
+    padding: 5px;
+  }
+`;
+
+export const Link = styled(ReactRouterLink)`
   color: #fff;
   text-decoration: none;
 
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+// useHistory is replaced with useNavigate
+import { useNavigate } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
 import FooterContainer from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
 import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Signin() {
-  const [emailAddress, setEmailAddress] = useState();
-  const [password, setPassword] = useState();
+  const history = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
   const isInvalid = password === "" || emailAddress === "";
@@ -13,6 +19,18 @@ export default function Signin() {
     event.preventDefault();
 
     // firebase work here!
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        // push to the browse page
+        history(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
   };
 
   return (
@@ -38,6 +56,13 @@ export default function Signin() {
               Sign In
             </Form.Submit>
           </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
         </Form>
       </HeaderContainer>
       <FooterContainer />
