79b2627555aab6c63100e93d11d3f85a7d91079d
Ashok2252 <ashokmali1999@gmail.com>
2023-10-31 10:50:04 +0530

    Player Component Added

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { SelectProfileContainer } from "./profiles";
 import FooterContainer from "./footer";
 import { FirebaseContext } from "../context/firebase";
-import { Loading, Header, Card } from "../components";
+import { Loading, Header, Card, Player } from "../components";
 import * as ROUTES from "../constants/routes";
 import logo from "../logo.svg";
 
@@ -101,10 +101,10 @@ export function BrowseContainer({ slides }) {
               ))}
             </Card.Entities>
             <Card.Feature category={category}>
-              {/* <Player>
+              <Player>
                 <Player.Button />
                 <Player.Video src="/videos/bunny.mp4" />
-              </Player> */}
+              </Player>
             </Card.Feature>
           </Card>
         ))}
